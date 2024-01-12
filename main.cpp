016350147aeeb085ed3846e9355e964b953b1b42
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-01-12 14:28:37 +0900

    ウィンドウ名を変更

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <Novice.h>
 #include "SceneManager.h"
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "PG2_STG";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
