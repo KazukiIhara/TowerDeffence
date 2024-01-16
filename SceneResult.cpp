0f92d3164133c192a0d6594b7c849648ed0b98f5
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-01-16 20:29:10 +0900

    resultSceneの追加

---
+++ SceneResult.cpp
@@ -0,0 +1,26 @@
+#include "SceneResult.h"
+
+cSceneResult::cSceneResult()
+{
+
+}
+
+cSceneResult::~cSceneResult()
+{
+
+}
+
+void cSceneResult::Init()
+{
+
+}
+
+void cSceneResult::Update(char* keys, char* preKeys)
+{
+	keys; preKeys;
+}
+
+void cSceneResult::Draw()
+{
+
+}
