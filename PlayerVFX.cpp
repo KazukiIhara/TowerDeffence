dd3f765500870d9168e55f53a3a3d66ee1e43fd4
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-02-02 14:49:01 +0900

    背景の色を変更 プレイヤーVFXクラスの作成

---
+++ PlayerVFX.cpp
@@ -0,0 +1,28 @@
+#include "PlayerVFX.h"
+#include <Novice.h>
+
+
+cPlayerVFX::cPlayerVFX()
+{
+
+}
+
+cPlayerVFX::~cPlayerVFX()
+{
+
+}
+
+void cPlayerVFX::Init()
+{
+
+}
+
+void cPlayerVFX::Update()
+{
+
+}
+
+void cPlayerVFX::Draw()
+{
+
+}
---
+++ PlayerVFX.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "MyIncludes.h"
+
+class cPlayerVFX
+{
+public:
+	cPlayerVFX();
+	~cPlayerVFX();
+	void Init();
+	void Update();
+	void Draw();
+
+private:
+
+};
+
